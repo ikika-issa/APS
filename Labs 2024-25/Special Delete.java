78e7797a6c003f7c4fc86f39b448c77388d0c9d7
Iskra Stojchevska <iskra.stojchevska@students.finki.ukim.mk>
2024-11-10 00:18:01 +0100

    Create Special Delete.java

---
+++ Labs 2024-25/Special Delete.java
@@ -0,0 +1,251 @@
+//You are given a single linked list with String nodes. 
+//Additionally, you are given one more integer L. You need to delete every node from the list that contains a String with less than L characters.
+
+import java.util.Scanner;
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class SLL<E> {
+    private SLLNode<E> first;
+
+    public SLL() {
+        // Construct an empty SLL
+        this.first = null;
+    }
+
+    public void deleteList() {
+        first = null;
+    }
+
+    public int size() {
+        int listSize = 0;
+        SLLNode<E> tmp = first;
+        while(tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret += tmp;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "->" + tmp;
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, null);
+        ins.succ = first;
+        //SLLNode<E> ins = new SLLNode<E>(o, first);
+        first = ins;
+    }
+
+    public void insertAfter(E o, SLLNode<E> node) {
+        if (node != null) {
+            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+            node.succ = ins;
+        } else {
+            System.out.println("Dadenot jazol e null");
+        }
+    }
+    public void insertBefore(E o, SLLNode<E> before) {
+
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if(first==before){
+                this.insertFirst(o);
+                return;
+            }
+            //ako first!=before
+            while (tmp.succ != before && tmp.succ!=null)
+                tmp = tmp.succ;
+            if (tmp.succ == before) {
+                tmp.succ = new SLLNode<E>(o, before);;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = new SLLNode<E>(o, null);
+        } else {
+            insertFirst(o);
+        }
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            first = first.succ;
+            return tmp.element;
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public E delete(SLLNode<E> node) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if(first == node) {
+                return this.deleteFirst();
+            }
+            while (tmp.succ != node && tmp.succ.succ != null)
+                tmp = tmp.succ;
+            if (tmp.succ == node) {
+                tmp.succ = tmp.succ.succ;
+                return node.element;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+                return null;
+            }
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+
+    }
+
+    public SLLNode<E> getFirst() {
+        return first;
+    }
+
+    public SLLNode<E> find(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void merge (SLL<E> in){
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while(tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = in.getFirst();
+        }
+        else{
+            first = in.getFirst();
+        }
+    }
+
+    public void mirror() {
+        if (first != null) {
+            //m=nextsucc, p=tmp,q=next
+            SLLNode<E> tmp = first;
+            SLLNode<E> newsucc = null;
+            SLLNode<E> next;
+
+            while(tmp != null){
+                next = tmp.succ;
+                tmp.succ = newsucc;
+                newsucc = tmp;
+                tmp = next;
+            }
+            first = newsucc;
+        }
+    }
+}
+
+public class NodeLength<E> {
+
+    //TODO: implement method
+    public static void specialDelete(SLL<String> list, int m) {
+        SLLNode<String> head = list.getFirst();
+
+        //Edge case: if the list is empty or has only one node, do nothing
+        if (head == null || head.succ == null) {
+            return;
+        }
+
+        SLLNode<String> current = head;
+        SLLNode<String> lastMatch = null;
+        SLLNode<String> lastMatchPrev = null;
+        SLLNode<String> prev = null;
+
+        // Traverse to find the last node with a string of length m
+        while (current != null) {
+            if (current.element.length() == m) {
+                lastMatch = current;
+                lastMatchPrev = prev;
+            }
+            prev = current;
+            current = current.succ;
+        }
+
+        //If no matching node found, return
+        if (lastMatch == null) {
+            return;
+        }
+
+        //If the last matching node is already at the head, no need to move
+        if(lastMatch == head) {
+            return;
+        }
+
+        //Detach the last matching node from its current position
+        if (lastMatchPrev != null) {
+            lastMatchPrev.succ = lastMatch.succ;
+        }
+
+        //Move the last matching node to the front
+        lastMatch.succ = head;
+        list.insertFirst(lastMatch.element);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        SLL<String> list = new SLL<>();
+
+        for(int i = 0; i < n; i++){
+            list.insertLast(input.next());
+        }
+
+        int m = input.nextInt();
+        System.out.println(list);
+
+        specialDelete(list, m);
+        System.out.println(list);
+    }
+
+}
